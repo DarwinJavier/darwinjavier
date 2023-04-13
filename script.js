d99d1750a3515824d1639e8f5ee31eebeb81a7c3
DarwinJavier <56559363+DarwinJavier@users.noreply.github.com>
2023-04-13 07:57:52 -0500

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('Hello!');
+console.log('Ups Ecuador');
+var i = 0;
+var elemento = document.getElementById('titulo1');
+elemento.addEventListener('click', function () {
+  alert('Click en titulo');
+  elemento.innerHTML = i;
+});
+
+elemento.addEventListener('mouseover', function () {
+  i = i + 1;
+  console.log(i);
+});
+
+var listas = document.getElementsByTagName('li');
+console.log(listas);
